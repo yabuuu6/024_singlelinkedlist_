8987a56109c5223743d5e91955d6fc67de3e7165
buyaramadhan <ytchochote@gmail.com>
2024-04-26 14:42:50 +0700

    commit 11

--- test1.cpp/test1.cpp.cpp
+++ test1.cpp/test1.cpp.cpp
@@ -151,4 +151,24 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
 			
