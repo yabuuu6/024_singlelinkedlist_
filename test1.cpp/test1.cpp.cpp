4dbdd9a411d0dfed28a732ff5558efed46b9b15a
buyaramadhan <ytchochote@gmail.com>
2024-04-26 14:42:33 +0700

    commit 10

--- test1.cpp/test1.cpp.cpp
+++ test1.cpp/test1.cpp.cpp
@@ -144,4 +144,11 @@ int main() {
 			cout << "Pilihan: ";
 			cin >> pilihan;
 			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
 			
