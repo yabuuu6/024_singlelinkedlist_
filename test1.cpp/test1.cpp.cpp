334881899f914b31028c9d81b3217325bb6e2f64
buyaramadhan <ytchochote@gmail.com>
2024-04-26 14:41:13 +0700

    commit 6

--- test1.cpp/test1.cpp.cpp
+++ test1.cpp/test1.cpp.cpp
@@ -90,3 +90,19 @@ bool listEmpty() {
 
 }
 
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
